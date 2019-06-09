c8db781030d5e4660d19c899e31deda6cdc9681d
Munyiwamwangi <josseymunyi@gmail.com>
2019-06-09 17:36:55 +0300

    changed boostrap file, current file failed to respond.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
-// $('div.scripting h4').mouse_click(function){
-//   $('p').toggler();
-// };
-$('.scripting h4').ready((function){
-  $('p').toggle(
-    function(){$('p').hide()};
-    function(){$('p').show()});
+//--------SCRIPT FOR SECTION4/WHAT WE DO PART-----
+
+// case1
+$('.p1').hide()
+$('#p1').click(function(){
+  $('.p1').toggle();
+  $('.p1').click(function(){
+    $('#p1').toggle();
+  });
+});
+
+
+// case2
+$('.p2').hide()
+$('#p2').click(function(){
+  $('.p2').toggle();
+  $('.p2').click(function(){
+    $('#p2').toggle();
+  });
+});
+
+
+// case3
+$('.p3').hide()
+$('#p3').click(function(){
+  $('.p3').toggle();
+  $('.p3').click(function(){
+    $('#p3').toggle();
+  });
 });
