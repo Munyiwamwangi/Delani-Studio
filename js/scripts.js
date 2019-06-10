b3a9bf1af3a658ab9ad555cdd247443a555abfa7
Munyiwamwangi <josseymunyi@gmail.com>
2019-06-10 19:36:37 +0300

    done formating business logic part, proceeding to front-end. Hovering properties fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,66 @@
+// BACK END; BUSINESS LOGIC
 //--------SCRIPT FOR SECTION4/WHAT WE DO PART-----
+$(document).ready(function() {
+  $(".p1").hide();
+  $("#p1").click(function() {
+    $(".p1").slideToggle();
+    $(".p1").click(function() {
+      $("#p1").slideToggle();
+    });
+  });
 
-// case1
-$('.p1').hide()
-$('#p1').click(function(){
-  $('.p1').toggle();
-  $('.p1').click(function(){
-    $('#p1').toggle();
+  // case2
+  $(".p2").hide();
+  $("#p2").click(function() {
+    $(".p2").slideToggle();
+    $(".p2").click(function() {
+      $("#p2").slideToggle();
+    });
+  });
+
+  // case3
+  $(".p3").hide();
+  $("#p3").click(function() {
+    $(".p3").slideToggle();
+    $(".p3").click(function() {
+      $("#p3").slideToggle();
+    });
   });
 });
 
-
-// case2
-$('.p2').hide()
-$('#p2').click(function(){
-  $('.p2').toggle();
-  $('.p2').click(function(){
-    $('#p2').toggle();
+//section5
+$(document).ready(function() {
+  $(".one").hide();
+  $("#img1").mouseenter(function() {
+    $(".one").slideToggle(100);
+  });
+  $(".one").hide();
+  $("#img5").mouseenter(function() {
+    $(".five").slideToggle(100);
+  });
+  $(".one").hide();
+  $("#img2").mouseenter(function() {
+    $(".two").slideToggle(100);
+  });
+  $(".one").hide();
+  $("#img3").mouseenter(function() {
+    $(".three").slideToggle(100);
+  });
+  $(".one").hide();
+  $("#img4").mouseenter(function() {
+    $(".four").slideToggle(100);
+  });
+  $(".one").hide();
+  $("#img6").mouseenter(function() {
+    $(".six").slideToggle(100);
+  });
+  $(".one").hide();
+  $("#img7").mouseenter(function() {
+    $(".seven").slideToggle(100);
+  });
+  $(".one").hide();
+  $("#img8").mouseenter(function() {
+    $(".eight").slideToggle(100);
   });
 });
-
-
-// case3
-$('.p3').hide()
-$('#p3').click(function(){
-  $('.p3').toggle();
-  $('.p3').click(function(){
-    $('#p3').toggle();
-  });
-});
+//FRONT END// UI
